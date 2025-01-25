3ac355ef40cfb6b7b7a9e6039d71c43a3a4d9ece
LOG1997 <2694233102@qq.com>
2025-01-25 21:17:20 +0800

    feat: 04 done

--- exercises/04_traits/07_deref/src/lib.rs
+++ exercises/04_traits/07_deref/src/lib.rs
@@ -12,11 +12,14 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn title(&self) -> &str {
-        todo!()
+        // 去除开头和结尾空格
+        self.title.trim_end()
+            .trim_start()
     }
 
     pub fn description(&self) -> &str {
-        todo!()
+        self.description.trim_end()
+            .trim_start()
     }
 }
 
